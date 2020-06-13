c8c0aed9200acc134ec089b8203b7ec92aa346fe
Liang Jia Sheng <liangsheng4@gmail.com>
2020-06-13 13:48:00 +0100

    currency

---
+++ precache-manifest.c1bb446b72a95b0681a464de895aac74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a475fd513c41b947f09e7d21b462ccc7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e4b89c1028fb61d11874",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "be62d6191b9ff16951f7",
+    "url": "/static/js/2.6ddc13cc.chunk.js"
+  },
+  {
+    "revision": "d09ac505fec0e9130a1d5b85fb3fa6dc",
+    "url": "/static/js/2.6ddc13cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4b89c1028fb61d11874",
+    "url": "/static/js/main.0c4355ab.chunk.js"
+  },
+  {
+    "revision": "ffe597a35f88a2ad344f",
+    "url": "/static/js/runtime-main.b060334e.js"
+  }
+]);
